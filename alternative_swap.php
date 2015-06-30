fa04c1d8576262f39f6ba7716f6948c1bc0973bf
Yusuf Americianaki <yusuf.americianaki@nist.gov>
2015-06-30 14:21:38 -0400

    Coding Conventions Met and made code easier to read

--- alternative_swap.php
+++ alternative_swap.php
@@ -1,48 +1,61 @@
 <?php
+header ( 'Content-Type: text/plain' );
+header ( 'Content-Disposition: attachment; filename=' . $_POST ["newfilename"] );
 
-header('Content-Type: text/plain');
-header('Content-Disposition: attachment; filename='.$_POST["newfilename"]);
+$original_file = str_split ( file_get_contents ( $_POST ['url'] ) );
 
-$original_file = str_split(file_get_contents($_POST['url']));
-//$modified_file = fopen($file_name, "w");
+if ( isset ( $_POST [ 'lettersDDMenu' ] ) ) {
+    $letter = $_POST [ 'lettersDDMenu' ];
+}
+else {
+    $letter = 1;
+    die ( "Something went wrong! You did not click on any letter from the drop down menu" );
+}
 
 
-(isset($_POST["let_select"])) ? $letter = $_POST["let_select"] : $letter=1;
+if ( $letter == "All Letters" ) {
+    swapAllCases ( $original_file );
+}
+else {
+    swapSingleCase ( $original_file , $letter );
+}
 
-if($letter == "All Letters"){
-    foreach($original_file as $value){
-        if(ord($value)>=65 && ord($value)<=90){ //capital version of letters
-            //fwrite($modified_file, chr(ord($value) + 32));
-            echo chr(ord($value) + 32);
+function swapAllCases ( $string_file )
+{
+    // ASCII values of capital and lowercase letters
+    $captialA = 65;
+    $captalZ = 90;
+    $lowercaseA = 97;
+    $lowercaseZ = 122;
+    foreach ( $string_file as $value ) {
+        $ASCII_of_char = ord ( $value );
+        if ( $ASCII_of_char >= $captialA && $ASCII_of_char <= $captalZ ) { // capital version of letters
+            echo chr( $ASCII_of_char + 32 );
         }
-        elseif (ord($value)>=97 && ord($value)<=122){ //lowercase version of letters
-            //fwrite($modified_file, chr(ord($value) - 32));
-            echo chr(ord($value) - 32);
+        elseif ( $ASCII_of_char >= $lowercaseA && $ASCII_of_char <= $lowercaseZ ) { // lowercase version of letters
+            echo chr( $ASCII_of_char - 32 );
         }
-        else{
-            //fwrite($modified_file, $value);
+        else {
             echo $value;
         }
     }
 }
-else{
-    $Capv_letter = $letter;
-    $Lowv_letter = chr(ord($letter) + 32);
-    foreach($original_file as $value){
-        if($value == $Capv_letter){
-            //fwrite($modified_file, chr(ord($value) + 32));
-            echo chr(ord($value) + 32);
+
+function swapSingleCase ( $string_file , $the_letter ) {
+    $Capv_letter = $the_letter;
+    echo "The Cav_Letter = " . $Capv_letter;
+    $Lowv_letter = chr ( ord ( $the_letter ) + 32 );
+    echo "The Low_Letter = " . $Lowv_letter;
+    foreach ( $string_file as $value ) {
+        $ASCII_of_char = ord ( $value );
+        if ( $value == $Capv_letter ) {
+            echo chr ( $ASCII_of_char + 32 );
         }
-        elseif($value == $Lowv_letter){
-            //fwrite($modified_file, chr(ord($value) - 32));
-            echo chr(ord($value) - 32);
+        elseif ( $value == $Lowv_letter ) {
+            echo chr ( $ASCII_of_char - 32 );
         }
-        else{
-            //fwrite($modified_file, $value);
+        else {
             echo $value;
         }
     }
 }
-//echo "Success! File was Succesfully Created";
-//fclose($modified_file);
-?>
