ce4dfde4f1881c60c83688c3d6c70975393bd4a2
Yusuf Americianaki <yusuf.americianaki@nist.gov>
2015-07-02 10:17:52 -0400

    Met MediaWiki quality standards for removing spaces after function call

--- alternative_swap.php
+++ alternative_swap.php
@@ -1,34 +1,34 @@
 <?php
-header ( 'Content-Type: text/plain' );
-header ( 'Content-Disposition: attachment; filename=' . $_POST ["newfilename"] );
+header( 'Content-Type: text/plain' );
+header( 'Content-Disposition: attachment; filename=' . $_POST["newfilename"] );
 
-$original_file = str_split ( file_get_contents ( $_POST ['url'] ) );
+$original_file = str_split( file_get_contents( $_POST['url'] ) );
 
-if ( isset ( $_POST [ 'lettersDDMenu' ] ) ) {
-    $letter = $_POST [ 'lettersDDMenu' ];
+if ( isset( $_POST[ 'lettersDDMenu' ] ) ) {
+    $letter = $_POST[ 'lettersDDMenu' ];
 }
 else {
     $letter = 1;
-    die ( "Something went wrong! You did not click on any letter from the drop down menu" );
+    die( "Something went wrong! You did not click on any letter from the drop down menu" );
 }
 
 
 if ( $letter == "All Letters" ) {
-    swapAllCases ( $original_file );
+    swapAllCases( $original_file );
 }
 else {
-    swapSingleCase ( $original_file , $letter );
+    swapSingleCase( $original_file , $letter );
 }
 
-function swapAllCases ( $string_file )
+function swapAllCases( $string_file )
 {
     // ASCII values of capital and lowercase letters
     $capitalA = 65;
     $capitalZ = 90;
     $lowercaseA = 97;
     $lowercaseZ = 122;
-    foreach ( $string_file as $value ) {
-        $ASCII_of_char = ord ( $value );
+    foreach( $string_file as $value ) {
+        $ASCII_of_char = ord( $value );
         if ( $ASCII_of_char >= $capitalA && $ASCII_of_char <= $capitalZ ) {
             echo chr( $ASCII_of_char + 32 );
         }
@@ -41,16 +41,16 @@ function swapAllCases ( $string_file )
     }
 }
 
-function swapSingleCase ( $string_file , $the_letter ) {
+function swapSingleCase( $string_file , $the_letter ) {
     $Capv_letter = $the_letter;
-    $Lowv_letter = chr ( ord ( $the_letter ) + 32 );
+    $Lowv_letter = chr( ord( $the_letter ) + 32 );
     foreach ( $string_file as $value ) {
-        $ASCII_of_char = ord ( $value );
+        $ASCII_of_char = ord( $value );
         if ( $value == $Capv_letter ) {
-            echo chr ( $ASCII_of_char + 32 );
+            echo chr( $ASCII_of_char + 32 );
         }
         elseif ( $value == $Lowv_letter ) {
-            echo chr ( $ASCII_of_char - 32 );
+            echo chr( $ASCII_of_char - 32 );
         }
         else {
             echo $value;
