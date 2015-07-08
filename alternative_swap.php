145187f6d4381e565f5c44dea3aa5c492bef5b73
Yusuf Americianaki <yusuf.americianaki@nist.gov>
2015-07-08 11:23:27 -0400

    added swapCase and constants

--- alternative_swap.php
+++ alternative_swap.php
@@ -12,6 +12,12 @@ else {
     die( "Something went wrong! You did not click on any letter from the drop down menu" );
 }
 
+// ASCII values of capital and lowercase letters
+define( "CAPITALA" , '65' );
+define( "CAPITALZ" , '90' );
+define( "LOWERCASEA" , '97' );
+define( "LOWERCASEZ" , '122' );
+define( "ASCIICASEDIF" , '32'); // the difference in ASCII values from one char case to another (e.g. ord( 'a' ) - ord( 'A' ) = 32)
 
 if ( $letter == "All Letters" ) {
     swapAllCases( $original_file );
@@ -22,18 +28,10 @@ else {
 
 function swapAllCases( $string_file )
 {
-    // ASCII values of capital and lowercase letters
-    $capitalA = 65;
-    $capitalZ = 90;
-    $lowercaseA = 97;
-    $lowercaseZ = 122;
     foreach( $string_file as $value ) {
         $ASCII_of_char = ord( $value );
-        if ( $ASCII_of_char >= $capitalA && $ASCII_of_char <= $capitalZ ) {
-            echo chr( $ASCII_of_char + 32 );
-        }
-        elseif ( $ASCII_of_char >= $lowercaseA && $ASCII_of_char <= $lowercaseZ ) {
-            echo chr( $ASCII_of_char - 32 );
+        if ( $ASCII_of_char >= CAPITALA && $ASCII_of_char <= LOWERCASEZ ) { // checks to see if the char is a char in the alphabet
+            swapCase ( $value );
         }
         else {
             echo $value;
@@ -43,17 +41,24 @@ function swapAllCases( $string_file )
 
 function swapSingleCase( $string_file , $the_letter ) {
     $Capv_letter = $the_letter;
-    $Lowv_letter = chr( ord( $the_letter ) + 32 );
+    $Lowv_letter = swapCase( $Capv_letter );
     foreach ( $string_file as $value ) {
-        $ASCII_of_char = ord( $value );
-        if ( $value == $Capv_letter ) {
-            echo chr( $ASCII_of_char + 32 );
-        }
-        elseif ( $value == $Lowv_letter ) {
-            echo chr( $ASCII_of_char - 32 );
+        if ( $value == $Capv_letter or $value == $Lowv_letter ) {
+            swapCase( $value );
         }
         else {
             echo $value;
         }
+    }
+}
+
+function swapCase( $character )
+{
+    $ASCII_of_char = ord( $character );
+    if ( $ASCII_of_char >= CAPITALA && $ASCII_of_char <= CAPITALZ) {
+        echo chr($ASCII_of_char + ASCIICASEDIF);
+    }
+    elseif ( $ASCII_of_char >= LOWERCASEA && $ASCII_of_char <= LOWERCASEZ ) {
+        echo chr( $ASCII_of_char - ASCIICASEDIF );
     }
 }
